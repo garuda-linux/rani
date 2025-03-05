ad6b0e593eacb7e8b3e54df8479c6f3f1705e7fb
TNE <tne@garudalinux.org>
2025-03-05 19:14:22 +0100

    refactor(darkmode): Redo darkmode to be more consistent

--- src/app/theme-handler/theme-handler.ts
+++ src/app/theme-handler/theme-handler.ts
@@ -1,4 +1,4 @@
-import { inject } from '@angular/core';
+import { effect, inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { CatppuccinScrollbars } from '../theme';
 import { Logger } from '../logging/logging';
@@ -10,42 +10,24 @@ export class ThemeHandler {
   private readonly logger = Logger.getInstance();
 
   constructor() {
-    this.checkDarkMode();
+    effect(() => {
+      const darkmode = this.configService.settings().darkMode;
+      void this.setDarkMode(darkmode);
+    });
   }
 
   /**
    * Toggle dark mode, updating the local storage and the document accordingly.
    */
-  public async toggleDarkMode(): Promise<void> {
-    await this.configService.updateConfig('darkMode', !this.configService.settings().darkMode);
-
-    this.document.documentElement.classList.toggle('p-dark', this.configService.settings().darkMode);
-    this.document.documentElement.style.scrollbarColor = this.configService.settings().darkMode
+  public setDarkMode(darkmode: boolean): void {
+    this.document.documentElement.classList.toggle('p-dark', darkmode);
+    this.document.documentElement.style.scrollbarColor = darkmode
       ? CatppuccinScrollbars.dark
       : CatppuccinScrollbars.light;
-    this.document.documentElement.style.backgroundColor = this.configService.settings().darkMode
+    this.document.documentElement.style.backgroundColor = darkmode
       ? '#1e1e2e'
       : '#eff1f5';
 
-    this.logger.debug(`Dark mode ${this.configService.settings().darkMode ? 'enabled' : 'disabled'}`);
-  }
-
-  /**
-   * Check if dark mode is enabled and set it accordingly.
-   * @private
-   */
-  private checkDarkMode(): void {
-    if (
-      localStorage.getItem('darkMode') === 'true' ||
-      (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      if (!this.configService.settings().darkMode) {
-        void this.toggleDarkMode();
-      }
-    } else {
-      if (this.configService.settings().darkMode) {
-        void this.toggleDarkMode();
-      }
-    }
+    this.logger.debug(`Dark mode ${darkmode ? 'enabled' : 'disabled'}`);
   }
 }
