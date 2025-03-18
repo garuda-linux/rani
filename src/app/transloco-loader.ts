5f1134974f654e492720010a4111e028edeb5690
Nico Jensch <root@dr460nf1r3.org>
2025-03-18 11:59:53 +0100

    feat(translation): use inbuilt Tauri resources to load translations

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -1,12 +1,12 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import type { Translation, TranslocoLoader } from '@jsverse/transloco';
-import { HttpClient } from '@angular/common/http';
+import { resolveResource } from '@tauri-apps/api/path';
+import { readTextFile } from '@tauri-apps/plugin-fs';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
-  private http = inject(HttpClient);
-
-  getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+  async getTranslation(lang: string): Promise<Translation> {
+    const resourcePath: string = await resolveResource(`../assets/i18n/${lang}.json`);
+    return JSON.parse(await readTextFile(resourcePath)) as Translation;
   }
 }
