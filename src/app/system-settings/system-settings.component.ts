f2a805471bd0d96e37af0485b980cc3d87c47e74
zoeruda <zoeruda@linuxmail.org>
2025-03-25 17:59:22 -0400

    fix(system-settings): correct 'service' to 'pkg' for "Thermald installed" checkbox

--- src/app/system-settings/system-settings.component.ts
+++ src/app/system-settings/system-settings.component.ts
@@ -135,7 +135,7 @@ export class SystemSettingsComponent {
           fancyTitle: 'systemSettings.powersave.thermald.title',
           description: 'systemSettings.powersave.thermald.description',
           checked: false,
-          check: { type: 'service', name: 'thermald.service' },
+          check: { type: 'pkg', name: 'thermald' },
         },
         {
           name: 'enabled-thermald',
