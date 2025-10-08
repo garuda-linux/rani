a6b48a4de27fda69438f9395cfbbbda121ddaba2
TNE <tne@garudalinux.org>
2025-10-08 03:00:26 +0200

    feat(shell.nix): Replace with actual working shell.nix

--- shell.nix
+++ shell.nix
@@ -1,2 +1,14 @@
-(import ./default.nix).devShells.${builtins.currentSystem}.default
-  or (throw "dev-shell not defined. Cannot find flake attribute devShell.${builtins.currentSystem}.default")
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSEnv {
+  name = "electron-env";
+  targetPkgs = pkgs: (with pkgs;
+    [
+      nodejs pnpm nspr glib nss dbus atk cups cairo gtk3 pango libgbm expat libxkbcommon alsa-lib udev libGL
+    ]
+  ) ++ (with pkgs.xorg;
+    [
+      libXrender libXdamage libXext libXfixes libXrandr libX11 libXcomposite libxcb
+    ]
+  );
+}).env
